18b28a76d7d7da5c3048d6c46758cc7abbf9a8e5
1612-00 <nguyenducanh.ldb@gmail.com>
2021-06-27 09:15:19 +0700

    Handle register and redirect between register-form and dashboard

--- client/src/components/auth/RegisterForm.js
+++ client/src/components/auth/RegisterForm.js
@@ -1,17 +1,60 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import AlertMessage from "./../layout/AlertMessage";
 
 const RegisterForm = () => {
+  // Local state
+  const [registerForm, setRegisterForm] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [alert, setAlert] = useState(null);
+
+  // Context
+  const { registerUser } = useContext(AuthContext);
+
+  const { username, password, confirmPassword } = registerForm;
+
+  const onChangeRegisterForm = (e) =>
+    setRegisterForm({ ...registerForm, [e.target.name]: e.target.value });
+
+  // Login
+  const register = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setAlert({ type: "danger", message: "Password is not match" });
+      setTimeout(() => setAlert(null), 4000);
+    }
+
+    try {
+      const registerData = await registerUser(registerForm);
+      if (!registerData.success) {
+        setAlert({ type: "danger", message: registerData.message });
+        setTimeout(() => setAlert(null), 4000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <Form className="my-3">
+      <Form className="my-3" onSubmit={register}>
+        <AlertMessage info={alert} />
         <Form.Group className="my-3">
           <Form.Control
             type="text"
             placeholder="Username"
             name="username"
             require
+            value={username}
+            onChange={onChangeRegisterForm}
           />
         </Form.Group>
         <Form.Group className="my-3">
@@ -20,6 +63,8 @@ const RegisterForm = () => {
             placeholder="Password"
             name="password"
             require
+            value={password}
+            onChange={onChangeRegisterForm}
           />
         </Form.Group>
         <Form.Group className="my-3">
@@ -28,6 +73,8 @@ const RegisterForm = () => {
             placeholder="Confirm Password"
             name="confirmPassword"
             require
+            value={confirmPassword}
+            onChange={onChangeRegisterForm}
           />
         </Form.Group>
         <Button variant="success" type="submit" className="mt-2">
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -74,8 +74,27 @@ const AuthContextProvider = ({ children }) => {
     }
   };
 
+
+  // Register
+  const registerUser = async (userForm) => {
+    try {
+      const response = await axios.post(`${apiUrl}/auth/register`, userForm);
+      if (response.data.success)
+        localStorage.setItem(
+          LOCAL_STORAGE_TOKEN_NAME,
+          response.data.accessToken
+        );
+
+      loadUser();
+      return response.data;
+    } catch (error) {
+      if (error.response.data) return error.response.data;
+      else return { success: false, message: error.message };
+    }
+  };
+
   // context data
-  const authContextData = { loginUser, authState };
+  const authContextData = { loginUser, registerUser, authState };
 
   // return provider
   return (
